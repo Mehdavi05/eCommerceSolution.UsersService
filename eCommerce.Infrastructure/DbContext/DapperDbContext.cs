b76e14540152a5a8710698c0491a51c1ba266670
Shujaat Hussain <shujaat.apple05@gmail.com>
2026-03-14 09:31:03 +0400

    Adding changes for the project to work on Docker Composer.

--- eCommerce.Infrastructure/DbContext/DapperDbContext.cs
+++ eCommerce.Infrastructure/DbContext/DapperDbContext.cs
@@ -12,7 +12,28 @@ public class DapperDbContext
     public DapperDbContext(IConfiguration configuration)
     {
         _configuration = configuration;
-        string? connectionString = _configuration.GetConnectionString("PostgresConnection");
+        
+        var host = Environment.GetEnvironmentVariable("POSTGRES_HOST");
+        var user = Environment.GetEnvironmentVariable("POSTGRES_USER");
+        var password = Environment.GetEnvironmentVariable("POSTGRES_PASSWORD");
+        var dbName =  Environment.GetEnvironmentVariable("POSTGRES_DATABASE_NAME");
+        
+        string? connectionString;
+        
+        Console.WriteLine($"HOST: {host} -- USER: {user} -- PASS: {password}  -- DB NAME: {dbName}");
+
+        if (string.IsNullOrEmpty(host) || string.IsNullOrEmpty(user) || string.IsNullOrEmpty(password) || string.IsNullOrEmpty(dbName))
+        {
+            connectionString = _configuration.GetConnectionString("PostgresConnectionValuesHardcoded");
+        }
+        else
+        {
+            connectionString = _configuration.GetConnectionString("PostgresConnectionValuesFromEnvVariables")
+                ?.Replace("$POSTGRES_HOST", host)
+                .Replace("$POSTGRES_USER", user)
+                .Replace("$POSTGRES_PASSWORD", password)
+                .Replace("$POSTGRES_DATABASE_NAME", dbName);;
+        }
 
         //Create a new NpgsqlConnection with the retrieved connection string
         _connection = new NpgsqlConnection(connectionString);
